c98afc74c7c3e0f6bd0077be406da4234cc09da2
Felipe da Silva dos Santos <felipe.silva@somospi.com.br>
2020-08-26 22:46:17 -0300

    adiciona nova estrutura para typescript

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,29 +1,18 @@
-import { Request, Response } from 'express';
-import EmailService from '../services/EmailService';
+import { Request, Response } from 'express'
+import { User } from '../schemas/User'
 
-const users = [
-  { name: 'felps', email: 'felpsdev03@gmail.com' },
-];
-
-export default {
-  async index(req: Request, res: Response) {
-    return res.json(users);
-  },
-
-  async create(req: Request, res: Response) {
-    const emailService = new EmailService();
-
-    emailService.sendMail({
-      to: { 
-        name: 'Felipe Santos', 
-        email: 'felpsdev03@gmail.com'
-      },
-      message: { 
-        subject: 'Bem-vindo ao sistema', 
-        body: 'Seja bem-vindo'
-      }
-    });
-
-    return res.send();
+class UserController {
+  public async index (req: Request, res: Response): Promise<Response> {
+    const users = await User.find()
+    
+    return res.json(users)
   }
-};
+
+  public async store (req: Request, res: Response): Promise<Response> {
+    const user = await User.create(req.body)
+
+    return res.json(user)
+  }
+}
+
+export default new UserController()
